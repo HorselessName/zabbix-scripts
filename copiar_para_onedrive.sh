4de4de9b54f806c2ce1762da15c07a3b7db46b04
Raul Chiarella <RaulPotter11@Gmail.com>
2022-01-07 09:21:25 -0300

    cronjob

--- copiar_para_onedrive.sh
+++ copiar_para_onedrive.sh
@@ -1,9 +1,24 @@
+# Crontab:
+# Todo domingo, uma hora
+# 0 1 * * 0 su - -c "/root/copiar_para_onedrive.sh > /dev/null 2> /dev/null"
+
 # Enviar BKP local p/ OneDrive
 # Ferramenta utilizada: https://github.com/HorselessName/onedrive
 
-BKP_DIR="/root/backup"
-ONEDRIVE_DIR="/root/onedrive_files/zabbix_bkp"
+BKP_DIR="$HOME/backup"
+ONEDRIVE_DIR="$HOME/onedrive_files/zabbix_bkp"
 # Sincroniza os arquivos de uma pasta com outra, ignorando caso o arquivo já exista
 rsync -a -v --ignore-existing $BKP_DIR/ $ONEDRIVE_DIR/
 # Faz upload dos arquivos sincronizados para o OneDrive e libera o espaço em disco local utilizado
-onedrive --synchronize --upload-only --local-first
+# Atençao, o arquivo de configuração deve estar configurado da seguinte forma:
+
+# Configuração do OneDrive:
+# mkdir -p $HOME/.config/onedrive/
+# wget -O $HOME/.config/onedrive/config https://raw.githubusercontent.com/abraunegg/onedrive/master/config
+# vi $HOME/.config/onedrive/config
+# sync_dir = "$HOME/onedrive_files"
+# upload_only = "true"
+# local_first = "true"
+# no_remote_delete = "true"
+
+onedrive --synchronize
